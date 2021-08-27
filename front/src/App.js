df5566df61872a11388809a88353f0b229e2f730
Dahlton <dahlton@linkd.dev>
2021-08-26 23:40:38 -0400

    get front page set up in react

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Body } from './components/Body';
+import { Navbar } from './components/layout/Navbar';
 
 function App() {
   return (
+    <Router >
     <div className="App">
-
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Body}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './fonts/Shalma-Rp0Pe.otf'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
