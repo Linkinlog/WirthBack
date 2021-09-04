16ad0422dd7c648ba8aa1a7ef30dd85c6a8d1e7d
Dahlton <dahlton@linkd.dev>
2021-09-03 20:07:11 -0400

    started users routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+
+/* GET all users page. */
+router.get('/', async (req, res, next) => {
+	const users = await User.find({});
+	res.json({ users });
+});
+
+
+// Register user
+router.post('/', [check('username', 'Username is required').not().isEmpty(), check('password', 'Password is required').isLength({ min: 6 })], async (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+
+	const { username } = req.body;
+
+	try {
+		let user = await User.findOne({ username });
+
+		if (user) {
+			return res.status(400).json({ msg: 'User already exists' });
+		}
+		const newUser = new User({ ...req.body });
+    const salt = await bcrypt.genSalt(10);
+		newUser.password = await bcrypt.hash(req.body.password, salt);
+		await newUser.save();
+	} catch (error) {
+		console.error(error);
+		return res.status(401).send('error');
+	}
+	res.json({ msg: 'Saved' });
 });
 
 module.exports = router;
