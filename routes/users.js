ebdb01487f2b999e3375651b6bebec82ad0f7453
Dahlton <dahlton@linkd.dev>
2021-09-04 23:32:14 -0400

    Fixed auth and perms for get users route

--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,33 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const auth = require('../middleware/auth');
 
-
-
-
-/* GET all users page. */
-router.get('/', async (req, res, next) => {
+//@GET '/'
+// Get all users
+// Private
+router.get('/', auth, async (req, res, next) => {
 	const users = await User.find({});
 	res.json({ users });
 });
 
-
+// @POST '/'
 // Register user
+// Public
 router.post('/', [check('username', 'Username and password are required').not().isEmpty(), check('password', 'Password and password are required').isLength({ min: 6 })], async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(400).json({ errors: errors.array() });
 	}
-
 	const { username } = req.body;
-
 	try {
 		let user = await User.findOne({ username });
-
 		if (user) {
 			return res.status(400).json({ msg: 'User already exists' });
 		}
 		const newUser = new User({ ...req.body });
-    const salt = await bcrypt.genSalt(10);
+		const salt = await bcrypt.genSalt(10);
 		newUser.password = await bcrypt.hash(req.body.password, salt);
 		await newUser.save();
 	} catch (error) {
